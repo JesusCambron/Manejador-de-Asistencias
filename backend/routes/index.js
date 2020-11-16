55e0b2c28d0a35a0908d3938d3dec76e83915107
Jesús Manuel Cambrón Tapia <manuel.cam98@gmail.com>
2020-11-16 16:40:12 -0700

    administrar archivos

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,12 @@ const usuarioRouter = require("./usuarios");
 const cursosRouter = require("./cursos");
 const gruposRouter = require("./grupos");
 const alumnosRouter = require("./alumnos");
+const archivosRouter = require("./archivos");
 
 routerIndex.use("/usuario", usuarioRouter);
 routerIndex.use("/cursos", cursosRouter);
 routerIndex.use("/grupos", gruposRouter);
 routerIndex.use("/alumnos", alumnosRouter);
+routerIndex.use("/archivos", archivosRouter);
 
 module.exports = routerIndex;
